cb01acf68386786b382f7068573f237601ca85d7
Maria Anna San Luis <maansanluis@gmail.com>
2018-11-03 00:12:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(theArray, addToArray){
 }
 
 function addElementToEndOfArray(theArray, addToArray){
-
+  var returnArray = [...theArray, addToArray]
 }
 
 function destructivelyAddElementToEndOfArray(theArray, addToArray){
