fc5f792ac5ad03f15fa06fdb683bbd9528be41d3
Maria Anna San Luis <maansanluis@gmail.com>
2018-11-03 00:34:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function accessElementInArray(theArray, theIndex){
 
 function destructivelyRemoveElementFromEndOfArray(theArray){
   theArray.shift(-1)
-  return theArray.shift(-1)
+  return theArray
 }
 
 function removeElementFromBeginningOfArray(theArray){
